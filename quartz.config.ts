b8724ad1d0bac6b7b3deda2b9fb693be58e92fa2
Seanie-D <sank-junior-purple@duck.com>
2026-04-08 11:12:55 -0500

    Quartz sync: Apr 8, 2026, 11:12 AM

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,7 +8,7 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "Frostpunk - A Nimble TTRPG Campaign",
+    pageTitle: "Frostpunk",
     pageTitleSuffix: "",
     enableSPA: true,
     enablePopovers: true,
